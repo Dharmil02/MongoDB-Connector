c22ee95b7d6ce86011488a9077c793b6d9e95118
Dharmil Malodiya <dharmilmalodiya@gmail.com>
2025-07-22 15:02:04 +0530

    latest update

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
 from setuptools import setup, find_packages
 from typing import List
-
-HYPEN_E_DOT='-e .'
-
-'''def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements'''
-
    
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
 __version__ = "0.0.6"
-REPO_NAME = "mongodb_connector"
-PKG_NAME= "MongoConnect"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+REPO_NAME = "MongoDB-Connector"
+PKG_NAME= "Database-Connection-PKG"
+AUTHOR_USER_NAME = "Dharmil02"
+AUTHOR_EMAIL = "dharmilmalodiya@gmail.com"
 
 setup(
     name=PKG_NAME,
@@ -38,6 +25,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    
-    
+    install_requires=["pymongo","pymongo[srv]","dnspython","pandas","numpy","ensure","pytest"],
 )
